99838a71e73ac7b97957d6fb61ecc49b28ebe7b5
Roger evangx <roge_oz@hotmail.com>
2015-10-27 21:37:05 -0600

    File Proveedores_c_c.java restored

--- Proveedores_c_c.java
+++ Proveedores_c_c.java
@@ -1,82 +1,106 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.applet.Applet.*;
 import java.applet.*;
-import javax.swing.*;
-import java.sql.*;
+import java.applet.Applet.*;
 import RhmTools.*;
-public class Clientes_r extends XFrame implements ActionListener{
-TextField t1;
-float c;
-TextField t2;
-TextField t3;
-TextField t4;
-TextField t5;
-Label l1;
-Label l2;
-Label l3;
-Label l4;
-Label l5;
+import java.sql.*;
+import java.util.*;
+import javax.swing.*;
+public class Proveedores_c_c extends XFrame implements ActionListener{
+Label l;
+TextField t;
+Button b, bc;
+Frame anterior;
 Statement st;
-int cc;
-Button b;
+ResultSet rs;
+Button reg;
+Label efx=new Label();
+Label egx=new Label();
+Label ehx=new Label();
+Label eix=new Label();
+
 
 public void start(){
-t4.setText("");
-t2.setText("");
-t3.setText("");
+t.setText("");
+}
+
+public Proveedores_c_c(Frame f){
+anterior=f;
+ex();
+}
+
+
+public void ex(){
+
+setSize(200,200);
+setLayout(null);
+anterior.setVisible(false);
+setVisible(false);
+setUndecorated(true);
+setVisible(true);
+setSize(400, 400);
+setLocation((Toolkit.getDefaultToolkit().getScreenSize().width/2)-200, 50);
+setBackground(Color.gray);
+bc=new Button("Cerrar");
+bc.addActionListener(this);
+l = new Label("ingrese la clave del proveedor");
+t= new TextField(6);
+b= new Button ("Consultar");
+b.addActionListener(this);
+add(l);
+add(t);
+add(b);
+l.setBounds(40, 90, 90, 23);
+t.setBounds(150, 90, 130, 23);
+b.setBounds(180, 120, 50, 23);
+reg= new Button("Cerrar");
+reg.setBounds(180, 30, 50, 23);
+reg.addActionListener(this);
+add(reg);
+add(efx);
+add(egx);
+add(ehx);
+efx.setBounds(20, 140, 380, 26);
+egx.setBounds(20, 166, 380, 26);
+ehx.setBounds(20, 190, 380, 26);
+
+}
+
+
+public void actionPerformed(ActionEvent e){
 if(makeConnection()){
+if(e.getSource()==b){
 try{
 st=cn.createStatement();
-String sql="select count(clave) as nombre from clientes";
-ResultSet rs=st.executeQuery(sql);
-System.out.println();
-while(rs.next()){
-String a=rs.getInt(1)+"";
-a=a.replace("SQLServerResultSet:","");
-cc=Integer.parseInt(a)+1;
-t1.setText("Cl-"+cc);
+rs=st.executeQuery("select * from proveedores where clavepr='"+t.getText()+"'");
+if(rs.next()){
+String eax="Clave: "+rs.getString(1);
+String ebx="Nombre: "+rs.getString(2);
+String ecx="Direccion: "+rs.getString(3);
+efx.setText(eax);
+egx.setText(ebx);
+ehx.setText(ecx);
+
+
+repaint();
+validate();
+}
+else{
+javax.swing.JOptionPane.showMessageDialog(null,"No se ha encontrado ningun proveedor con esa clave", "Error", javax.swing.JOptionPane.ERROR_MESSAGE);
+start();
 }
 }
 catch(Exception ez){
 System.out.println(ez.getMessage());
 }
 }
-else{}
-}
-
-
-public void actionPerformed(ActionEvent e){
-if(e.getSource()==b){
-if(makeConnection()){
-if(!t1.getText().equals("") & !t2.getText().equals("") & !t3.getText().equals("") & !t4.getText().equals("")){
-String a=t1.getText();
-String b=t2.getText();
-try{
-c=Float.parseFloat(t3.getText());
-}catch(Exception x){
-javax.swing.JOptionPane.showMessageDialog(null,"Tipo de dato incorrecto en el campo adeudo debe ser valor numerico","Error",javax.swing.JOptionPane.ERROR_MESSAGE);
-
-}
-String d=t4.getText();
-try{
-det dez = new det();
-if (dez.name(t2.getText(), 30) & dez.name(t4.getText(), 30)){
-st=cn.createStatement();
-String sql="insert into clientes values('";
-sql+=a;
-sql+="', ";
-sql+=c+", '"+b+"', '"+d;
-sql+="')";
-int eaz=st.executeUpdate(sql);
-javax.swing.JOptionPane.showMessageDialog(null,"Se han creado "+eaz+" registros en la base de datos","informacion",javax.swing.JOptionPane.INFORMATION_MESSAGE);
-start();
 }
 else{
-javax.swing.JOptionPane.showMessageDialog(null,"Favor de introducir caracteres validos en Nombre y direccion", "Error",javax.swing.JOptionPane.INFORMATION_MESSAGE);
+javax.swing.JOptionPane.showMessageDialog(null,"Error al conectar a la base de datos, reinicie el componente, o contacte al administrador", "Error", javax.swing.JOptionPane.ERROR_MESSAGE);
+}
+if(e.getSource()==reg){
+setVisible(false);
+anterior.setVisible(true);
 }
 }
-catch(SQLException exz){
-javax.swing.JOptionPane.showMessageDialog(null,"Ha ocurrido un error, favor de checar que la clave no ha sido ingresada ya con anterioridad  \n si el problema persiste contacte con el administrador \nError: "+exz.getSQLState()+""+exz.getMessage(), "Error",javax.swing.JOptionPane.INFORMATION_MESSAGE);
 }
-catch(Exception 
